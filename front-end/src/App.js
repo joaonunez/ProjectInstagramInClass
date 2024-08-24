8c73435f86cd18b549864fe99df0d4402c49819d
joaonunez <joaovaldiglesias@gmail.com>
2024-08-24 13:22:03 -0400

    back and front added

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom"
+function App() {
+  return (
+    <>
+      <Router>
+        <Route path="/" element={}/>
+      </Router>
+    </>
+  );
+
+}
+
+export default App;
